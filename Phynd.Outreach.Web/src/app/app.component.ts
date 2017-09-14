980d16ab151de22218d0134b4f7cdef8bbed67e8
ashwinb <ashwinb@xpanxion.co.in>
2017-09-14 15:58:18 +0530

    Commit for angular2 and Kendo migration with dot net core

---
+++ Phynd.Outreach.Web/src/app/app.component.ts
@@ -0,0 +1,20 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    title = 'app';
+    //onButtonClick() {
+    //    this.title = 'Hello from Kendo UI!';
+    //}
+    public events: string[] = [];
+    public source: Array<string> = ["Albania", "Andorra", "Armenia", "Austria", "Azerbaijan"];
+    public data: Array<string>;
+
+    constructor() {
+        this.data = this.source.slice();
+    }
+}
---
+++ Phynd.Outreach.Web/src/app/app.module.ts
@@ -0,0 +1,23 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpModule, JsonpModule } from '@angular/http';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { AppComponent } from './app.component';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpModule,
+        JsonpModule,
+        DropDownsModule
+    ],
+    declarations: [
+        AppComponent
+    ],
+    bootstrap: [
+        AppComponent
+    ]
+})
+export class AppModule { }
